6c92371551c652ef8eb879fdea0786188fa54377
adhoc-king <46354827+adhoc-king@users.noreply.github.com>
2019-01-07 20:28:10 +0530

    Create bigint.cpp

---
+++ bigint.cpp
@@ -0,0 +1,25 @@
+/**
+*    Username : adhoc_king
+*    Name : adhoc_king
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define scani(n) scanf("%d",&n);
+#define EPS 1e-9
+#define FOR(i,n) for(int i=0; i<n; i++)
+#define X first
+#define Y second
+#define arrinput for(int i=0; i<n; i++) cin>>arr[i];
+#define sumarr for(int i=0; i<n; i++) sum+=arr[i];
+#define memarr memset(arr, 0, sizeof(arr));
+#define inp0(arr,n) for(int i=0; i<n; i++) cin>>arr[i];
+#define sum0(arr,n) for(int i=0; i<n; i++) sum+=arr[i];
+#define mem0(arr) memset(arr, 0, sizeof(arr));
+#define for0(i,n) for(int i=0; i<n; i++)
+#define for1(i,n) for(int i=1; i<=n; i++)
+#define forr(i,n) for(int i=n-1; i>=0; i--)
+
