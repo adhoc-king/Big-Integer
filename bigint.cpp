c5f28306dfe5d2f3389b96cc3de2ae0466e47690
adhoc-king <46354827+adhoc-king@users.noreply.github.com>
2019-01-08 11:46:32 +0530

    Update bigint.cpp

--- bigint.cpp
+++ bigint.cpp
@@ -13,12 +13,6 @@ using namespace std;
 #define FOR(i,n) for(int i=0; i<n; i++)
 #define X first
 #define Y second
-#define arrinput for(int i=0; i<n; i++) cin>>arr[i];
-#define sumarr for(int i=0; i<n; i++) sum+=arr[i];
-#define memarr memset(arr, 0, sizeof(arr));
-#define inp0(arr,n) for(int i=0; i<n; i++) cin>>arr[i];
-#define sum0(arr,n) for(int i=0; i<n; i++) sum+=arr[i];
-#define mem0(arr) memset(arr, 0, sizeof(arr));
 #define for0(i,n) for(int i=0; i<n; i++)
 #define for1(i,n) for(int i=1; i<=n; i++)
 #define forr(i,n) for(int i=n-1; i>=0; i--)
