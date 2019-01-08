e64e518ed98070fef0f5ffefa6faa042d0f6b669
adhoc-king <46354827+adhoc-king@users.noreply.github.com>
2019-01-08 15:15:44 +0530

    Update bigint.cpp

--- bigint.cpp
+++ bigint.cpp
@@ -1,7 +1,6 @@
 /**
 *    Username : adhoc_king
 *    Name : adhoc_king
-*    Institute : IIT Bombay
 */
 
 // #include <bits/stdc++.h>
@@ -13,19 +12,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <string>
-// #include <deque>
-// #include <queue>
-// #include <array>
-// #include <ctime>
-// #include <list>
-// #include <map>
-// #include <set>
-// #include <cassert>
-// #include <complex>
-// #include <sstream>
-// #include <forward_list>
-// #include <unordered_set>
-// #include <unordered_map>
 using namespace std;
 
 #define pb push_back
@@ -38,9 +24,6 @@ using namespace std;
 #define arrinput for(int i=0; i<n; i++) cin>>arr[i];
 #define sumarr for(int i=0; i<n; i++) sum+=arr[i];
 #define memarr memset(arr, 0, sizeof(arr));
-#define inp0(arr,n) for(int i=0; i<n; i++) cin>>arr[i];
-#define sum0(arr,n) for(int i=0; i<n; i++) sum+=arr[i];
-#define mem0(arr) memset(arr, 0, sizeof(arr));
 #define for0(i,n) for(int i=0; i<n; i++)
 #define for1(i,n) for(int i=1; i<=n; i++)
 #define forr(i,n) for(int i=n-1; i>=0; i--)
@@ -52,13 +35,11 @@ typedef vector<pii> vii;
 typedef vector<int> BigInt;
 
 
- 
-
 class BigInteger {
 
 private:
 
-	void Set(BigInt &vec) {
+    void Set(BigInt &vec) {
         while(vec.size()>1 && vec.back()==0) vec.pop_back();
     }
     void Print(BigInt vec) {
@@ -86,7 +67,7 @@ public:
 			ans.insert(ans.begin(), x);
 		}
 		Set(ans);
-        anoopam = ans;
+        	anoopam = ans;
 		//return ans;
 	}
 
